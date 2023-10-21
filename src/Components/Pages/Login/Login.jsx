dbb49dc8afb93097e79da057db23466c109676df
PrasadRanjane32 <prasadranjane32@gmail.com>
2023-10-21 18:45:36 +0530

    Forget Password

--- src/Components/Pages/Login/Login.jsx
+++ src/Components/Pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useContext,  useEffect,  useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './Login.css';
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
@@ -9,9 +9,9 @@ import { UserContext } from '../../../App'
 
 
 const Login = ({ isUserVerified,
-  setUserVerified ,setVeryPage,VeryPage}) => {
-  const {state, dispatch} = useContext(UserContext);
-  const navigate =useNavigate()
+  setUserVerified, setVeryPage, VeryPage }) => {
+  const { state, dispatch } = useContext(UserContext);
+  const navigate = useNavigate()
   const notify = (message) => {
     toast.error(message, {
       position: "top-right",
@@ -24,7 +24,7 @@ const Login = ({ isUserVerified,
       theme: "dark",
     });
   };
-  
+
   const successnotify = (message) => {
     toast.success(message, {
       position: "top-right",
@@ -49,11 +49,11 @@ const Login = ({ isUserVerified,
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     try {
       const response = await fetch("https://angry-moon-10536.pktriot.net/login", {
         method: "POST",
-        credentials:'include',
+        credentials: 'include',
         headers: {
           "Content-Type": "application/json",
           "Access-Control-Allow-Origin": '*',
@@ -67,7 +67,7 @@ const Login = ({ isUserVerified,
         notifyToast("Invalid Credentials", "error");
       } else if (response.status === 200) {
         const data = await response.json();
-       
+
         if (data.userVerified) {
           setUserVerified(true);
           dispatch({ type: "USER", payload: true });
@@ -93,7 +93,7 @@ const Login = ({ isUserVerified,
       console.error("Error:", error);
     }
   };
-  
+
   const handleSendVerificationEmail = async () => {
     try {
       const res = await fetch("https://angry-moon-10536.pktriot.net/send-verification-email", {
@@ -106,10 +106,10 @@ const Login = ({ isUserVerified,
 
       if (res.status === 201) {
         successnotify("Verification email sent");
-      }else if(res.status === 200){
+      } else if (res.status === 200) {
         successnotify("Email Is Already Verified");
         navigate('/');
-      }else {
+      } else {
         notify("Failed to send verification email", "Error");
       }
     } catch (error) {
@@ -132,10 +132,10 @@ const Login = ({ isUserVerified,
         successnotify("Email verified successfully");
         navigate('/');
 
-      } else if(res.status === 200){
+      } else if (res.status === 200) {
         notify("Email Is Already Verified");
         navigate('/');
-      }else {
+      } else {
         notify("Failed to verify email", "Error");
       }
     } catch (error) {
@@ -143,115 +143,116 @@ const Login = ({ isUserVerified,
       notify("Failed to verify email", "Error");
     }
   };
- 
+
   useEffect(() => {
     const fetchUserType = async () => {
-        const res = await fetch("https://angry-moon-10536.pktriot.net/fetchUserType", {
-          method: "GET",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json"
-          },
-          withCredntials: true,
-          credentials: 'include',
-        });
-        console.log(res);
+      const res = await fetch("https://angry-moon-10536.pktriot.net/fetchUserType", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        withCredntials: true,
+        credentials: 'include',
+      });
+      console.log(res);
 
-        if(state){
-            notify("User Logged In")
-            navigate('/')
-            console.error("Unauthorized Access Denied");
-            notify("Unauthorized Access Denied", "error");
-        }
-        else if (res.status===401) {
-          console.log("Status 401",res);
-        
-        }
-     
+      if (state) {
+        notify("User Logged In")
+        navigate('/')
+        console.error("Unauthorized Access Denied");
+        notify("Unauthorized Access Denied", "error");
+      }
+      else if (res.status === 401) {
+        console.log("Status 401", res);
+
+      }
+
     };
 
     fetchUserType(); // Call the async function
   }, [navigate]);
   return (
-   <>
-    <div className='login'>
-      <div className='row'>
-      <div className=' col-4'>
-      <div className={VeryPage ? "d-none" : ""}>
-      <div className="login-box form-group">
-        <h2>Login</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="user-box">
+    <>
+      <div className='login'>
+        <div className='row'>
+          <div className=' col-4'>
+            <div className={VeryPage ? "d-none" : ""}>
+              <div className="login-box form-group">
+                <h2>Login</h2>
+                <form onSubmit={handleSubmit}>
+                  <div className="user-box">
+                    <input
+                      type="text"
+                      name="username"
+                      className="form-control"
+                      required
+                      value={formData.username}
+                      onChange={handleChange}
+                      autoComplete="username"
+                    />
+                    <label>Email</label>
+                  </div>
+                  <div className="user-box">
+                    <input
+                      type="password"
+                      name="password"
+                      className="form-control"
+                      required
+                      value={formData.password}
+                      onChange={handleChange}
+                      autoComplete="current-password"
+                    />
+                    <label>Password</label>
+                  </div>
+                  <div className='lgn-btn'>
+                    <Link to="/register">
+                      SignUp
+                    </Link>
+                    <button type="submit" onClick={handleSubmit}>
+                      Login
+                    </button>
+                  </div>
+                  <div className='text-white text-center mt-4 m-0 p-0'>
+                    <p>Forget Password...?
+                      <Link className='h5' style={{ fontSize: '16px',textDecoration: 'none' }} to="/register">
+                        resetPassword
+                      </Link>
+                    </p>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={VeryPage ? "" : "d-none"}>
+          <div className="verification-container">
+            <h3>Verify Your Email</h3>
+            <p>Enter the verification token sent to your email:</p>
             <input
               type="text"
-              name="username"
               className="form-control"
-              required
-              value={formData.username}
+              name="emailVerificationToken"
+              placeholder="Enter Token"
+              value={formData.emailVerificationToken}
               onChange={handleChange}
-              autoComplete="username"
             />
-            <label>Email</label>
-          </div>
-          <div className="user-box">
-            <input
-              type="password"
-              name="password"
-              className="form-control"
-              required
-              value={formData.password}
-              onChange={handleChange}
-              autoComplete="current-password"
-            />
-            <label>Password</label>
-          </div>
-          <div className='lgn-btn'>
-             
-            
-            {/* <Link to="/register">
-              resetPassword
-            </Link> */}
-            <Link to="/register">
-              SignUp
-            </Link>
-            <button type="submit" onClick={handleSubmit}>
-             Login
+            <button
+              className="btn btn-verify half-form"
+              onClick={handleSendVerificationEmail}
+            >
+              Send Verify Email
             </button>
-         
+            <button
+              className="btn btn-verify half-form"
+              onClick={handleVerifyEmail}
+            >
+              Verify Email
+            </button>
           </div>
-        </form>
+        </div>
       </div>
-      </div>
-      </div>
-      </div>
-      <div className={VeryPage ? "" : "d-none"}>
-      <div className="verification-container">
-      <h3>Verify Your Email</h3>
-      <p>Enter the verification token sent to your email:</p>
-      <input
-        type="text"
-        className="form-control"
-        name="emailVerificationToken"
-        placeholder="Enter Token"
-        value={formData.emailVerificationToken}
-        onChange={handleChange}
-      />
-      <button
-        className="btn btn-verify half-form"
-        onClick={handleSendVerificationEmail}
-      >
-       Send Verify Email
-      </button>
-      <button
-        className="btn btn-verify half-form"
-        onClick={handleVerifyEmail}
-      >
-        Verify Email
-      </button>
-      </div>
-      </div>
-    </div>
-              </>
+    </>
   );
 };
 
